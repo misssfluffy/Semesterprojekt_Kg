9120cf2e02838110a18c275a17c7713b4d4a9966
misssfluffy <lailawerbung@yahoo.de>
2015-01-19 21:53:07 +0100

    bennenung für die gamemodes

--- COD/scripts.js
+++ COD/scripts.js
@@ -1,9 +1,141 @@
+/* noch für multiplayer zum klicken machen*/
 
+
+/* Search and rescue*/
 $(document).ready(function(){
 
 	$(".gap32, .gap4").hide();
-	$(".gap2").click(function(){
+	$(".gap21").click(function(){
 		$(".gap32, .gap4").fadeIn('500');
 
 	});
 });
+
+/* gungame*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap22").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* cranked*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap23").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* free f a*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap24").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* team deathm*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap25").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+
+/* search and destroy*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap26").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* domination*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap27").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* team tactical*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap28").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* infected*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap29").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* blitz*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap210").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* hunted free f a*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap211").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+/* ground war*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap212").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+
+/* drop zone*/
+$(document).ready(function(){
+
+	$(".gap32, .gap4").hide();
+	$(".gap213").click(function(){
+		$(".gap32, .gap4").fadeIn('500');
+
+	});
+});
+
+
+
+
+
+
+
