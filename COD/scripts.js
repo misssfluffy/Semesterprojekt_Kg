42ef021998b66aebf0287326412e29d743dac092
misssfluffy <lailawerbung@yahoo.de>
2015-01-26 17:42:51 +0100

    alle klicks

--- COD/scripts.js
+++ COD/scripts.js
@@ -1,12 +1,78 @@
 /* noch für multiplayer zum klicken machen*/
+$(document).ready(function(){
+
+	$(".gap2").hide();
+	var toggle = false;
+	$(".gap01").click(function(){
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap2").fadeIn('500');
+	
+
+		} else {
+			$(".gap2").fadeOut('500');
+			$(".gap2 ").fadeOut('500');
+			$(".gap2").fadeOut('500');
+			$(".gap2").fadeOut('500');
+			$(".gap2").fadeOut('500');
+			$(".gap2").fadeOut('500');
+			$(".gap2").fadeOut('500');
+
+			
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
+
+	});
+});
+
+$(document).ready(function(){
+
+	$(".gap2, .gap03").hide();
+	var toggle = false;
+	$(".gap02").click(function(){
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap03").fadeIn('500');
+	
+
+		} else {
+			$(".gap03").fadeOut('500');
+			
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
+
+	});
+});
 
 
 /* Search and rescue*/
 $(document).ready(function(){
 
 	$(".gap31, .gap41").hide();
+	var toggle = false;
 	$(".gap21").click(function(){
-		$(".gap31, .gap41").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap31, .gap41").fadeIn('500');
+			$(".gap22").addClass("luecke");
+
+		} else {
+			$(".gap31, .gap41").fadeOut('500');
+			$(".gap21").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -15,8 +81,22 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap32, .gap42").hide();
+	var toggle = false;
 	$(".gap22").click(function(){
-		$(".gap32, .gap42").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap32, .gap42").fadeIn('500');
+			$(".gap22").addClass("luecke");
+
+		} else {
+			$(".gap32, .gap42").fadeOut('500');
+			$(".gap22").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -25,8 +105,22 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap33, .gap43").hide();
+	var toggle = false;
 	$(".gap23").click(function(){
-		$(".gap33, .gap43").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap33, .gap43").fadeIn('500');
+			$(".gap23").addClass("luecke");
+
+		} else {
+			$(".gap33, .gap43").fadeOut('500');
+			$(".gap23").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -35,8 +129,22 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap34, .gap44").hide();
+	var toggle = false;
 	$(".gap24").click(function(){
-		$(".gap34, .gap44").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap34 .gap44").fadeIn('500');
+			$(".gap24").addClass("luecke");
+
+		} else {
+			$(".gap34, .gap44").fadeOut('500');
+			$(".gap24").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -45,29 +153,69 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap35, .gap45").hide();
+	var toggle = false;
 	$(".gap25").click(function(){
-		$(".gap35, .gap45").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap35, .gap45").fadeIn('500');
+			$(".gap25").addClass("luecke");
+
+		} else {
+			$(".gap35, .gap45").fadeOut('500');
+			$(".gap25").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
-
 
 /* search and destroy*/
 $(document).ready(function(){
 
 	$(".gap36, .gap46").hide();
+	var toggle = false;
 	$(".gap26").click(function(){
-		$(".gap36, .gap46").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap36, .gap46").fadeIn('500');
+			$(".gap26").addClass("luecke");
+
+		} else {
+			$(".gap36, .gap46").fadeOut('500');
+			$(".gap26").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
-
 /* domination*/
 $(document).ready(function(){
 
 	$(".gap37, .gap47").hide();
+	var toggle = false;
 	$(".gap27").click(function(){
-		$(".gap37, .gap47").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap37, .gap47").fadeIn('500');
+			$(".gap27").addClass("luecke");
+
+		} else {
+			$(".gap37, .gap47").fadeOut('500');
+			$(".gap27").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -76,8 +224,22 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap38, .gap48").hide();
+	var toggle = false;
 	$(".gap28").click(function(){
-		$(".gap38, .gap48").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap38, .gap48").fadeIn('500');
+			$(".gap28").addClass("luecke");
+
+		} else {
+			$(".gap38, .gap48").fadeOut('500');
+			$(".gap28").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -86,18 +248,45 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap39, .gap49").hide();
+	var toggle = false;
 	$(".gap29").click(function(){
-		$(".gap39, .gap49").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap39, .gap49").fadeIn('500');
+			$(".gap29").addClass("luecke");
+
+		} else {
+			$(".gap39, .gap49").fadeOut('500');
+			$(".gap29").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
-
 /* blitz*/
 $(document).ready(function(){
 
 	$(".gap310, .gap410").hide();
+	var toggle = false;
 	$(".gap210").click(function(){
-		$(".gap310, .gap410").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap310, .gap410").fadeIn('500');
+			$(".gap210").addClass("luecke");
+
+		} else {
+			$(".gap310, .gap410").fadeOut('500');
+			$(".gap210").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -106,8 +295,22 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap311, .gap411").hide();
+	var toggle = false;
 	$(".gap211").click(function(){
-		$(".gap311, .gap411").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap311, .gap411").fadeIn('500');
+			$(".gap211").addClass("luecke");
+
+		} else {
+			$(".gap311, .gap411").fadeOut('500');
+			$(".gap211").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -116,8 +319,22 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap312, .gap412").hide();
+	var toggle = false;
 	$(".gap212").click(function(){
-		$(".gap312, .gap412").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap312, .gap412").fadeIn('500');
+			$(".gap212").addClass("luecke");
+
+		} else {
+			$(".gap312, .gap412").fadeOut('500');
+			$(".gap212").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
@@ -127,13 +344,25 @@ $(document).ready(function(){
 $(document).ready(function(){
 
 	$(".gap313, .gap413").hide();
+	var toggle = false;
 	$(".gap213").click(function(){
-		$(".gap313, .gap413").fadeIn('500');
+		
+		console.log(toggle);
+		if (toggle) {
+			$(".gap313, .gap413").fadeIn('500');
+			$(".gap213").addClass("luecke");
+
+		} else {
+			$(".gap313, .gap413").fadeOut('500');
+			$(".gap213").removeClass("luecke");
+			 	
+		}
+		toggle = !toggle; 
+		console.log(toggle);
+			
 
 	});
 });
-
-
 
 
 
